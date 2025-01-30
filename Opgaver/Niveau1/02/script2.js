29078a1f96ecb749a18b0a620e7da8975e26718d
Julie Bissø <U2507981@rts-365.dk>
2025-01-30 11:26:04 +0100

    opg2 løst

---
+++ Opgaver/Niveau1/02/script2.js
@@ -0,0 +1,14 @@
+function beregnForhold (a, b){
+    return a/b
+}
+
+let resultat = beregnForhold(a,b)
+
+function beregnForhold2 (a, b){
+    return a/b
+}if(b == 0){
+    return null
+
+}
+
+console.log(resultat);
